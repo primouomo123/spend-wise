70ee4ac7368e94e8ac397f9460ddcac59abb483b
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-05-13 19:50:36 -0400

    app.py initialized (imports)

---
+++ server/app.py
@@ -0,0 +1,4 @@
+from flask import request, session, jsonify, make_response
+
+from config import app, db, api, jwt
+from models import User, Category, Transaction, Budget, UserSchema, CategorySchema, TransactionSchema, BudgetSchema
