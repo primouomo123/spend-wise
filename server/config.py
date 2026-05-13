5c5ae85488ed5fcc8df7c9c97af6780806004ed4
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-05-13 19:05:09 -0400

    Configuration file complete

---
+++ server/config.py
@@ -0,0 +1,32 @@
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+jwt = JWTManager(app)
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+bcrypt = Bcrypt(app)
+
+api = Api(app)
