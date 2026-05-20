ac7c79e20cac8374a49e1be14baff913fd485dab
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-05-20 09:22:18 -0400

    Decimal imports done

--- server/models/budget.py
+++ server/models/budget.py
@@ -1,3 +1,5 @@
+from decimal import Decimal, InvalidOperation
+
 from sqlalchemy import CheckConstraint
 from sqlalchemy.orm import validates as model_validates
 from marshmallow import Schema, fields, validate, validates as schema_validates, ValidationError, RAISE, pre_load, post_load
