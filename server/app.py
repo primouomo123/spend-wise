335fb8e9fa5e2c21fed98e7d2a7658331ab6f31c
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-05-23 09:40:46 -0400

    Updating imports

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
 from flask import request, session, jsonify, make_response
 
 from config import app, db, api, jwt
-from models import User, Category, Transaction, Budget, UserSchema, CategorySchema, TransactionSchema, BudgetSchema
+from models import User, Category, Transaction, Budget, UserSchema, CategorySchema, TransactionSchema, BudgetSchema, UserDetailSchema, CategoryDetailSchema, TransactionDetailSchema, BudgetDetailSchema
