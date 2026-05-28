b264cbeecefdf05c1e5b5a95db3cca9bdcfde431
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-05-28 17:12:03 -0400

    After logging in, default categories are created

--- server/resources/signup.py
+++ server/resources/signup.py
@@ -4,7 +4,7 @@ from sqlalchemy.exc import IntegrityError
 from flask_jwt_extended import create_access_token, create_refresh_token
 
 from config import db
-from models import User, CreateUserSchema
+from models import CreateUserSchema, Category
 from marshmallow import ValidationError
 
 class Signup(Resource):
@@ -24,6 +24,16 @@ class Signup(Resource):
 
             access_token = create_access_token(identity=str(user.id))
             refresh_token = create_refresh_token(identity=str(user.id))
+
+            if user:
+                category1 = Category(name='Income', user_id=user.id)
+                category2 = Category(name='Food', user_id=user.id)
+                category3 = Category(name='Others', user_id=user.id)
+                db.session.add_all([category1, category2, category3])
+                db.session.commit()
+
+                if not category1 or not category2 or not category3:
+                    return make_response(jsonify({'error': 'Failed to create default categories'}), 500)
 
             return make_response(jsonify({
                 'access_token': access_token,
