ddfec9659c08b92b75f0607b518904e65c7af4ac
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-05-29 11:26:13 -0400

    I've added a missing schema

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,4 +1,4 @@
 from .user import User, UserSchema, CreateUserSchema, UserDetailSchema
 from .category import Category, CategorySchema, CreateCategorySchema, UpdateCategorySchema, CategoryDetailSchema
-from .transaction import Transaction, TransactionSchema, CreateTransactionSchema, TransactionDetailSchema
+from .transaction import Transaction, TransactionSchema, CreateTransactionSchema, UpdateTransactionSchema, TransactionDetailSchema
 from .budget import Budget, BudgetSchema, BudgetDetailSchema
