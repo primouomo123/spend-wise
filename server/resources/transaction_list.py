e4ed39865c6bf2855836ec47f7a19e725aba46b9
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-05-29 00:07:57 -0400

    SUmmary endpoint file created for later implementation

--- server/resources/transaction_list.py
+++ server/resources/transaction_list.py
@@ -7,7 +7,10 @@ from marshmallow import ValidationError
 from models import Transaction, CreateTransactionSchema, TransactionDetailSchema
 from config import db
 
+from utils import get_exchange_rate
+
 class TransactionList(Resource):
     """Resource for listing all transactions and creating new transactions."""
     @jwt_required()
     def get(self):
+        user_id = get_jwt_identity()
