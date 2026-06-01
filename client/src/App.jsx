501873e80300258080d4eef4a6ec6c116b4a75bc
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-06-01 11:00:39 -0400

    Changed the signup and login routes location

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,9 +20,9 @@ function App() {
           <Route path="transactions" element={<Transactions />} />
           <Route path="budget" element={<Budget />} />
           <Route path="me" element={<Me />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
         </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   )
