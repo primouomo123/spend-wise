6b49525dfe9ebc328f96ae06ad68fc5c47a28cf3
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-06-01 08:09:39 -0400

    Eliminating refresh_token for now

--- server/config.py
+++ server/config.py
@@ -15,7 +15,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=15)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=1)
 app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
