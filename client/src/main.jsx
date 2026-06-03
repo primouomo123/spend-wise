e9742de57e06378b5d1c968ad1a4d538dcecee5f
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-06-03 19:04:04 -0400

    Transaction context finished

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { UserProvider } from './contexts/UserContext.jsx';
 import { CategoryProvider } from './contexts/CategoryContext.jsx';
+import { TransactionProvider } from './contexts/TransactionContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserProvider>
       <CategoryProvider>
-        <App />
+        <TransactionProvider>
+          <App />
+        </TransactionProvider>
       </CategoryProvider>
     </UserProvider>
   </StrictMode>,
